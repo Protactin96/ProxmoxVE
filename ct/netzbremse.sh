15e22c916d375983c56e491300db96978706b32a
Protactin96 <protactin9696@gmail.com>
2026-02-13 14:30:53 +0100

    Changed links for fork

--- ct/netzbremse.sh
+++ ct/netzbremse.sh
@@ -2,7 +2,7 @@
 source <(curl -fsSL https://raw.githubusercontent.com/Protactin96/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 tteck
 # Author: tteck (tteckster)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/Protactin96/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/AKVorrat/netzbremse-measurement
 #         https://github.com/lwndp/netzbremse-dashboard
 
